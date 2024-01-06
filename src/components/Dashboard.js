6f892d20856d6005021d2c9583086d54a23c6a78
officialprosingh <lionparwindersingh0@gmail.com>
2024-01-06 18:30:25 -0500

    new files

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Home from "../images/home@2x.png"
+import Home from "../images/home-icon.png"
 import Backpack from "../images/backpack@2x.png"
 import Health from "../images/heart-with-pulse@2x.png"
 import Map from "../images/map@2x.png"
 import Dossier from "../images/user@2x.png"
+import logo from "../images/logo.png";
+import Bio from "../images/bio.png"
+
 
 
 
@@ -15,40 +18,21 @@ function Dashboard () {
         <div className="ui container center aligned" style={{ marginTop:"100px"}}>
             <div className="ui grid">
                 <div className="four wide column">
-                    <div className="ui vertical menu">
-
-
-                        <div className="item">
-                            <img className="ui small image" src={Home} style={{ width:"50px", height:"50px"}}/>
-                            <div className="content">Home</div>
-                        </div>
-
-                        <div className="item">
-                            <img className="ui small image" src={Backpack} style={{ width:"50px", height:"50px"}}/>
-                            <div className="content">Backpack</div>
-                        </div>
-
-                        <div className="item">
-                            <img className="ui small image" src={Health} style={{ width:"50px", height:"50px"}}/>
-                            <div className="content">Health</div>
-                        </div>
-
-                        <div className="item">
-                            <img className="ui small image" src={Map} style={{ width:"50px", height:"50px"}}/>
-                            <div className="content">Map</div>
-                        </div>
-
-                        <div className="item">
-                            <img className="ui small image" src={Dossier} style={{ width:"50px", height:"50px"}}/>
-                            <div className="content">Dossier</div>
-                        </div>
-
-
+                    <div className="ui labeled icon vertical menu">
+                        <a className="item"><i className="home icon"></i> Home</a>
+                        <a className="item"><i className="suitcase icon"></i> Backpack</a>
+                        <a className="item"><i className="heartbeat icon"></i> Health</a>
+                        <a className="item"><i className="map icon"></i> Map</a>
+                        <a className="item"><i className="book icon"></i> Dossier</a>
                     </div>
                 </div>
+
                 <div className="twelve wide stretched column">
                     <div className="ui segment">
-                        <h1 className="ui header">Hi, Biologist!</h1>
+                        <h2 class="ui right floated header" style={{marginTop: "25px"}}>Hi Biologist!</h2>
+                        <img className="ui left floated header" src={Bio} style={{width: "80px"}}/>
+                        <div className="ui clearing divider"></div>
+
                     </div>
                 </div>
             </div>
