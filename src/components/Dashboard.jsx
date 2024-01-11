465d079fd74639f94d5e54d66af7e44f5f1b2d4f
Pro Singh <45476771+officialprosingh@users.noreply.github.com>
2024-01-11 14:48:34 -0500

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -58,7 +58,9 @@ function Dashboard () {
 
     return (
 
-        <div className="ui container center aligned" style={{ marginTop:"100px"}}>
+        <div className="ui container center aligned" style={{marginTop: "100px"}}>
+
+
             <div className="ui grid">
                 <div className="four wide column">
                     <div className="ui labeled icon vertical menu">
@@ -70,43 +72,50 @@ function Dashboard () {
                     </div>
                 </div>
 
-                <div className="twelve wide stretched column">
-                    <div className="ui segment">
-                        <h2 className="ui right floated header" style={{marginTop: "25px"}}>
-                            Hi {userData.name || 'Biologist'}!
-                        </h2>
-
-                        <img className="ui left floated header" src={Bio} style={{width: "80px"}}/>
-                        <div className="ui clearing divider"></div>
-
-                        <h4 className="ui right floated header">out of 100%</h4>
-                        <h4 className="ui right floated header">{userData.health}%</h4>                        <h4
-                        className="ui right floated header">Current Health:</h4>
-
-                        <h4 className="ui left floated header">Hunting Permit Day:</h4>
-                        <h4 className="ui left floated header">{userData.stage}</h4>
-
-                        <div className="ui clearing divider"></div>
-
-                        <div className="ui embed">
 
 
-                            <iframe width="760" height="415"
-                                    src="https://www.youtube-nocookie.com/embed/5-Hx-UVjBlk?controls=0"
-                                    title="YouTube video player" frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    allowFullScreen></iframe>
+            <div className="twelve wide stretched column">
+                <div className="ui segment">
+                    <h2 className="ui right floated header" style={{marginTop: "25px"}}>
+                        Hi {userData.name || 'Biologist'}!
+                    </h2>
 
-                        </div>
+                    <img className="ui left floated header" src={Bio} style={{width: "80px"}}/>
+                    <div className="ui clearing divider"></div>
 
-                        <div className="ui clearing divider"></div>
+                    <h4 className="ui right floated header">out of 100%</h4>
+                    <h4 className="ui right floated header">{userData.health}%</h4>                        <h4
+                    className="ui right floated header">Current Health:</h4>
 
-                        <button className="ui blue right floated button">Next</button>
+                    <h4 className="ui left floated header">Hunting Permit Day:</h4>
+                    <h4 className="ui left floated header">{userData.stage}</h4>
 
+                    <div className="ui clearing divider"></div>
+
+                    <div className="ui embed">
+
+
+                        <iframe width="760" height="415"
+                                src="https://www.youtube-nocookie.com/embed/5-Hx-UVjBlk?controls=0"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowFullScreen></iframe>
 
                     </div>
+
+                    <div className="ui clearing divider"></div>
+
+                    <button className="ui blue right floated button">
+
+                        <a href="/awakening" style={{ color:"White"}} >
+                            Next
+                        </a>
+                    </button>
+
+
                 </div>
             </div>
+        </div>
         </div>
 
 
